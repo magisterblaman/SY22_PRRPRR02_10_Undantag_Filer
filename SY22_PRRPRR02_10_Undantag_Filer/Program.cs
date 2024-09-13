416e98e37fdb87fd97e8e72e0b818c072dc1e982
magisterblaman <hannes.blaman@lbs.se>
2024-09-13 09:46:16 +0200

    Skriva till fil

--- SY22_PRRPRR02_10_Undantag_Filer/Program.cs
+++ SY22_PRRPRR02_10_Undantag_Filer/Program.cs
@@ -3,22 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SY22_PRRPRR02_10_Undantag_Filer {
 	internal class Program {
 		static void Main(string[] args) {
-			try {
-				Console.WriteLine("Skriv in ett tal");
-				string numInput = Console.ReadLine();
-				int num = int.Parse(numInput);
+			while (true) {
+				string content2 = Console.ReadLine();
 
-				int result = 100 / num;
-				Console.WriteLine("Nu ska jag dela med ditt tal utan att dubbelkolla det wooooo........");
-				Console.WriteLine(result);
-			} catch (FormatException e) {
-				Console.WriteLine("Något gick snett i konverteringen");
-			} catch (DivideByZeroException e) {
-				Console.WriteLine("Du skrev in en nolla din nolla");
+				// File.WriteAllText("test.txt", content2 + "\n");
+				File.AppendAllText("test.txt", content2 + "\n");
 			}
 		}
 	}
