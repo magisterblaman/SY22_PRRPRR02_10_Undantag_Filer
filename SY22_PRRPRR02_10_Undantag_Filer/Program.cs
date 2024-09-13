9d6f52996e25c6e18a0ff9366d056daef23e4c8f
magisterblaman <hannes.blaman@lbs.se>
2024-09-13 10:42:41 +0200

    StreamWriter

--- SY22_PRRPRR02_10_Undantag_Filer/Program.cs
+++ SY22_PRRPRR02_10_Undantag_Filer/Program.cs
@@ -8,16 +8,17 @@ using System.IO;
 namespace SY22_PRRPRR02_10_Undantag_Filer {
 	internal class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Skriv in ett filnamn");
-			string path = Console.ReadLine();
 
-			if (File.Exists(path)) {
-				string content = File.ReadAllText(path);
-				Console.WriteLine(content);
-			} else {
-				Console.WriteLine("Kunde inte hitta filen!");
-			}
+			FileStream fs = File.Open("streamdemo.txt", FileMode.Create);
+			StreamWriter writer = new StreamWriter(fs);
 
+			writer.WriteLine("Hejhejehjehej");
+			writer.WriteLine("dsaiojfoijads");
+			writer.WriteLine("ännu något mer");
+			writer.WriteLine("bajs");
+
+			writer.Close();
+			fs.Close(); // motsatt ordning från när vi skapade variablerna
 		}
 	}
 }
