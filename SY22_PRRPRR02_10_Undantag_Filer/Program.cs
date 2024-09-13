cb1e4601e3c641b97d9830c24ce3053c1790c226
magisterblaman <hannes.blaman@lbs.se>
2024-09-13 09:26:53 +0200

    Undantag

--- SY22_PRRPRR02_10_Undantag_Filer/Program.cs
+++ SY22_PRRPRR02_10_Undantag_Filer/Program.cs
@@ -7,6 +7,19 @@ using System.Threading.Tasks;
 namespace SY22_PRRPRR02_10_Undantag_Filer {
 	internal class Program {
 		static void Main(string[] args) {
+			try {
+				Console.WriteLine("Skriv in ett tal");
+				string numInput = Console.ReadLine();
+				int num = int.Parse(numInput);
+
+				int result = 100 / num;
+				Console.WriteLine("Nu ska jag dela med ditt tal utan att dubbelkolla det wooooo........");
+				Console.WriteLine(result);
+			} catch (FormatException e) {
+				Console.WriteLine("Något gick snett i konverteringen");
+			} catch (DivideByZeroException e) {
+				Console.WriteLine("Du skrev in en nolla din nolla");
+			}
 		}
 	}
 }
